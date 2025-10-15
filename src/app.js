fa9701593462977651e3584beca745ee79fe847b
SDQ-BQA <sdmbqa420@gmail.com>
2025-10-15 23:10:32 +0530

    add: routes and controller configure

--- src/app.js
+++ src/app.js
@@ -1,22 +1,28 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
-
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 
 // app.use(express.json({limit: "16kb"}))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-// file 
-app.use(express.static("public"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// file
+app.use(express.static("public"));
 // cookie
-app.use(cookieParser())
+app.use(cookieParser());
 
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// router declaration
+app.use("/api/v1/users", userRouter);
 
 export default app;
