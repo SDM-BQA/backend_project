d841db1cb8bddc2a1c42812140f13002b4e0a4f0
SDQ-BQA <sdmbqa420@gmail.com>
2025-10-16 07:59:07 +0530

    feat: register user

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -2,15 +2,14 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import {ApiResponse} from "../utils/ApiResponse.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
     // get user details
     const { fullName, username, email, password } = req.body;
-    console.log(fullName, username, email, password);
-    
+
     // if (fullName === "") throw new ApiError(400, "fullname is required");
-    
+
     // validation - not empty
     if (
         [fullName, email, username, password].some(
@@ -19,61 +18,73 @@ const registerUser = asyncHandler(async (req, res) => {
     ) {
         throw new ApiError(400, "All fields are required");
     }
-    
+
     // check if user already exists - username or email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }],
     });
-    
+
     if (existedUser) {
         throw new ApiError(
             409,
             "User with email or username is already exists"
         );
     }
-    
+
     // check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
-    
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImage) &&
+        req.files.coverImage.length > 0
+    ) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
     }
-    
+
     // upload them to cloudinary, avatar
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-    
+
     if (!avatar) {
-        throw new ApiError(400, "Avatar file is required");
+        throw new ApiError(400, "Avatar file - cloudinary is required");
     }
-    
-    
+
     // create user object - create entry in db
     const user = await User.create({
         fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
-        password, 
-        username: username.toLowerCase()
-    })
-    
+        password,
+        username: username.toLowerCase(),
+    });
+
     // remove password and refresh token from response
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
-    )
-    
+    );
+
     // check for user response
-    if (createdUser) {
-        throw new ApiError(500, "Something went wrong while registering the user")
+    if (!createdUser) {
+        throw new ApiError(
+            500,
+            "Something went wrong while registering the user"
+        );
     }
 
     // return response
-    return res.status(201).json(
-        new ApiResponse(200, createdUser, "User registered successfully")
-    )    
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(200, createdUser, "User registered successfully")
+        );
 });
-
 
 export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,8 @@
+import { config } from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+config()
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -10,13 +12,15 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
-        const response = cloudinary.uploader.upload(localFilePath, {
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto",
         });
 
         console.log("File Uploaded on Cloudinary", (await response).url);
         return response;
     } catch (error) {
+        console.log("Cloudinary Error", error.message);
+        
         fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
